69afbca5fb6e716850ea18d74e9486e8a34131e0
eng-hakimi12 <ahakimbashir0@gmail.com>
2024-07-28 13:02:16 +0300

    color

--- src/Component/Form.jsx
+++ src/Component/Form.jsx
@@ -1,10 +1,10 @@
 
 function Form({Close}) {
-    return <div className=" bg-[rgba(0, 0, 0, 0.5);] h-screen absolute w-full top-0">
+    return <div className="  bg-[rgba(0,0,0,0.5)] h-screen absolute w-full top-0">
         {/* <div>
             <h1 className="font-bold text-red-300 text-3xl mt-3">This is Form</h1>
         </div> */}
-        <div className="flex 	bg-neutral-200	background-color: rgb(229 229 229); justify-center pt-20">
+        <div className="flex 	 justify-center pt-20">
     <form className="mt-10   p-3 w-[400px] h-[450px]">
     <i onClick={Close}  class=" text-3xl fa-solid fa-x"></i>
         <input className="w-[360px] border-2 border-black my-5 h-[40px]  "  type="text" placeholder="Enter title" />
